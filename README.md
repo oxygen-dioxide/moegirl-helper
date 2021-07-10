4db012600d0de7ee1267313ca97b5c11c31fd392
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2021-07-10 09:10:29 +0800

    package.json

--- README.md
+++ README.md
@@ -13,6 +13,17 @@
 
 ## 使用方法
 
+## 补充模板提示
+由于只有我一人开发，本仓库目前只收录了少部分常用模板。大家可以按以下方式补充：
+1. 下载[模板提示表](https://github.com/oxygen-dioxide/moegirl-helper/blob/master/snippets/moegirl-snippets.xlsx)
+2. 按以下格式在表格最后一行后追加：
+    - 第一列为模板的最简短说明
+    - 第二列为模板名称
+    - 第三列为模板的完整格式，用${1:}、${2:}等符号代表用户输入的参数
+    - 第四列为模板的较详细说明
+    - 第一列以\开头的行为注释，可用于分隔
+3. [创建issue](https://github.com/oxygen-dioxide/moegirl-helper/issues/new/choose)，将你修改过的xlsx文件作为附件上传。
+
 ## 开发
 1. 安装[Nodejs](https://nodejs.org/) 10以上版本
 2. 下载本仓库并进入目录
--- snippets/moegirl-snippets.py
+++ snippets/moegirl-snippets.py
@@ -4,15 +4,16 @@ wb = load_workbook("moegirl-snippets.xlsx")
 ws=wb[wb.sheetnames[0]]
 snippet_dict={}
 for i in range(2,ws.max_row):
-    keys=ws.cell(row=i,column=1).value.split(";")
-    #print(keys)
-    if(keys[0][0]!="\\"):#以反斜杠开头的行视为注释
-        for key in keys:
-            body="{{"+ws.cell(row=i,column=2).value
-            if("\n" in body):
-                body=body.split("\n")
-            description=ws.cell(row=i,column=3).value
-            snippet_dict[key]={"prefix":"{{"+key,"body":body,"description":description}
+    key=ws.cell(row=i,column=1).value
+    if(key!=None and key[0]!="\\"):#以反斜杠开头的行视为注释
+        prefixs=ws.cell(row=i,column=2).value.split(";")
+        body="{{"+ws.cell(row=i,column=3).value
+        if("\n" in body):
+            body=body.split("\n")
+        description=ws.cell(row=i,column=4).value
+        #print(prefixs)
+        prefixs=["{{"+i for i in prefixs]
+        snippet_dict[key]={"prefix":prefixs,"body":body,"description":description}
 outstr=""
 line=""
 with open("snippets.yaml",encoding="utf8") as infile:
